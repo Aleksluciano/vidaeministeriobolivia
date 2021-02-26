2d1e0613f54deb602e807f6fcd9d961123dfb1f7
Alex <aleksluciano@gmail.com>
2021-02-25 23:54:29 -0300

    M

--- src/components/Designation.svelte
+++ src/components/Designation.svelte
@@ -64,13 +64,13 @@
     try {
       // console.log(params)
       // const res = await fetch("http://localhost:3000/jw?data=" + params);
-      const res = await fetch("https://southamerica-east1-vidaeministerio-e4bf6.cloudfunctions.net/data?data=" + params, {mode: 'no-cors'});
-       let dadosjs = await res.json();
-       dadosjs = dadosjs.data;
-       console.log(dadosjs);
-
-      //const res = await callFirebaseFnJw({ data: params });
-      //let dadosjs = res.data.dados; // get info layout from jw site
+      //const res = await fetch("https://southamerica-east1-vidaeministerio-e4bf6.cloudfunctions.net/data?data=" + params, {mode: 'no-cors'});
+       //let dadosjs = await res.json();
+       //dadosjs = dadosjs.data;
+       //console.log(dadosjs);
+      //
+      const res = await callFirebaseFnJw({ data: params });
+      let dadosjs = res.data.dados; // get info layout from jw site
       dadosjs = dadosjs.filter(a => a !== null);
      
    
